350f4cc06cb9402b7c458482d6fbde8eb615765a
jheinemeyer <jheinemeyer@users.noreply.github.com>
2018-01-14 13:27:56 -0700

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,7 @@
 client_id   = '' # your bot's client ID
 token       = '' # your bot's token
 postgresql  = 'postgresql://user:password@host/database' # your postgresql info from above
+
+# for debugging with Pandora
+default_user = ''
+default_pass = ''
